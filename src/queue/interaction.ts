30afbc9b6f049825c712034fac87d371af422292
mohamed-tarek-fouad <mt1348@fayoum.edu.eg>
2023-12-01 19:47:15 +0200

    fixes: check queue and guild

--- src/queue/interaction.ts
+++ src/queue/interaction.ts
@@ -1,16 +1,20 @@
-import { CacheType, GuildMember, Interaction } from "discord.js";
+import { CacheType, Guild, GuildMember, Interaction } from "discord.js";
 import { queueModel } from "./queue.model";
 
 const interaction = async (i: Interaction<CacheType>) => {
   if (i.isChatInputCommand()) {
     const member = i.member as GuildMember;
-
+    const guild = i.guild as Guild;
     const options = i.options;
     const gameName = options.get("game_name")?.value as string;
     const gameMode = options.get("game_mode")?.value as string;
     const gameRank = options.get("game_rank")?.value as string;
     const user = member.id;
-    const queue = await queueModel({ gameName, gameMode, gameRank }, user);
+    const queue = await queueModel(
+      { gameName, gameMode, gameRank },
+      user,
+      guild
+    );
     if (queue === "in queue") {
       i.reply(` You Are Already In Queue`);
     } else if (queue?.id) {
--- src/queue/queue.model.ts
+++ src/queue/queue.model.ts
@@ -1,8 +1,14 @@
 import prisma from "@/utils/db/db";
 import { QueueDetails } from "@/utils/types/queue";
+import { Guild } from "discord.js";
 
-export const queueModel = async (queueDetails: QueueDetails, user: string) => {
+export const queueModel = async (
+  queueDetails: QueueDetails,
+  user: string,
+  guild: Guild
+) => {
   try {
+    const { id, name } = guild;
     const { gameMode, gameName, gameRank } = queueDetails;
     const queue = await prisma.$transaction(async (prisma) => {
       let newUser = user;
@@ -11,15 +17,29 @@ export const queueModel = async (queueDetails: QueueDetails, user: string) => {
       });
       const validateQueue = validateuser
         ? await prisma.queue.findFirst({
-            where: { userId: newUser },
+            where: { userId: newUser, fulfilled: false },
           })
         : undefined;
       if (validateQueue) return "in queue";
       if (!validateuser) {
         newUser = (await prisma.user.create({ data: { id: user } })).id;
       }
+      const validateGuild = await prisma.guild.findUnique({
+        where: { id: guild.id },
+      });
+      if (!validateGuild) {
+        await prisma.guild.create({
+          data: { id, name },
+        });
+      }
       const joinQueue = await prisma.queue.create({
-        data: { gameMode, gameName, gameRank, userId: newUser },
+        data: {
+          gameMode,
+          gameName,
+          gameRank,
+          userId: newUser,
+          guildId: guild.id,
+        },
       });
       return joinQueue;
     });
